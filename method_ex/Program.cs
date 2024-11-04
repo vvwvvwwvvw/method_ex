e94833266d736da9830e6b3a74a1c90d46c7652a
남정욱 <wjddnr3304@gmail.com>
2024-11-04 13:11:00 +0900

    초기 커밋.

---
+++ method_ex/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace method_ex
+{
+     class Program
+    {
+        public static void methodByValue(int x)
+        {
+            x = x + 1;
+        }
+        public static void methodArray(int[] x)
+        {
+            for(int i = 0; i < 5; i++) x[i] = 1;
+        }
+        public static void methodRef(ref int x)
+        {
+            x= x + 1;
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            methodByValue(a);
+            Console.WriteLine(a);// 출력 1
+
+            int[] b = {0,1,2,3,4};
+            methodArray(b);
+            for(int i = 0;i < 5; i++)
+            {
+                Console.WriteLine(b[i]);// 츨력 2
+            }
+
+            methodRef(ref a);
+            Console.WriteLine(a);// 출력 3
+
+        }
+    }
+}
